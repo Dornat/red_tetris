90971f99a8a89dfa83a00203803c5bf9ac2f4bea
Yevhenii MARCHYSHYN <ymarchys@e1r8p13.unit.ua>
2019-10-26 17:36:16 +0300

    Fixed nickname state

--- client/components/Form/FormNickname.js
+++ client/components/Form/FormNickname.js
@@ -1,42 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {connect} from "react-redux";
 import {setUser} from "../../actions/userActions";
 
 const FormNickname = (props) => {
 
-    const [form, setValues] = useState({
-        user: props.user || ''
-    });
-
-    const [inputClass, setClass] = useState("nes-input nickname__input");
-
     const onSubmit = (e) => {
         e.preventDefault();
 
-        if (form.user.length) {
+        if (!props.form.user.length) {
+            props.setError(true);
+        }
+        else {
             props.setError(false);
-            setClass("nes-input nickname__input");
         }
-        props.setUser(form.user);
-    };
 
-    const onChange = (e) => {
-        setValues({
-            [e.target.name]: e.target.value,
-        });
+        props.setUser(props.form.user);
     };
-
-    useEffect(() => {
-        if (props.isError) {
-            setClass("nes-input is-error nickname__input")
-        }
-    });
 
     return (
         <form className="form__nickname" onSubmit={event => onSubmit(event)}>
             <div className="input__group">
                 <label className="input__label">Enter your nickname</label>
-                <input type="text" name="user" className={inputClass} onChange={event => onChange(event)} value={form.user}/>
+                <input type="text" name="user"
+                    className={props.isError ? "nes-input is-error nickname__input" : "nes-input nickname__input"}
+                    onChange={event => props.onChange(event)} value={props.form.user}
+                />
                 <input type="submit" className="nes-btn is-primary" value="Save"/>
             </div>
         </form>
