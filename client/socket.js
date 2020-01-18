4ea0d52686ffa35968e0e07b1fda3f68ae5c3def
Dornat <DjDornat1@mail.ru>
2020-01-18 19:26:57 +0200

    Added host environment variable.

--- client/socket.js
+++ client/socket.js
@@ -6,7 +6,7 @@ import io from 'socket.io-client';
 
 export const initSocketConnection = () => {
     try {
-        return io.connect('http://localhost:' + process.env.IO_SERVER_PORT);
+        return io.connect('http://' + process.env.HOST + ':' + process.env.IO_SERVER_PORT);
     } catch (e) {
         console.log(e);
     }
