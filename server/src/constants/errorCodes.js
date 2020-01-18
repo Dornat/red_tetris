a9de437602615f59eef7472154923f29381d7a96
Dornat <DjDornat1@mail.ru>
2020-01-18 18:40:19 +0200

    Production ready building system was implemented.

---
+++ server/src/constants/errorCodes.js
@@ -0,0 +1,11 @@
+export const MODAL_NO_ROOM = 1;
+export const MODAL_NO_SPACE = 2;
+export const MODAL_ROOM_JOINED = 3;
+export const MODAL_GAME_PAUSED = 4;
+export const MODAL_GAME_OVER = 5;
+
+export const MSG_JOINED_ROOM = 1;
+export const MSG_PLAYER_ADDED = 2;
+export const MSG_GAME_CREATED = 3;
+export const ERROR_ROOM_NOT_FOUND = 4;
+export const ERROR_NO_SPACE_AVAILABLE = 5;
